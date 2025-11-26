ca854263510fda3003ef856b308a2dc2217d511e
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-11-26 20:52:24 +0530

    pallindrome

---
+++ Lect27/code1.c
@@ -0,0 +1,29 @@
+// Make Pallindrome by removing last characters
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[]="madamghj";
+    int i,j;
+    int length = strlen(s);
+    int pallin[length];
+
+    for(i=0,j=length-1;i<j;i++,j--)
+    {
+        if(s[i]!=s[j])
+        {
+            i=0,length--,j=length-1;
+        }
+
+    }
+
+    for(i=0;i<length;i++)
+    {
+        pallin[i]=s[i];
+        printf("%c",pallin[i]);
+    }
+
+
+}
